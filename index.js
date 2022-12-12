b0d20cb13934b16ff7cbdfc947b864c00a12776c
Victor Sandoval <distraido_99@hotmail.com>
2022-12-12 12:18:01 -0600

    Exportar desde otro archivo

---
+++ index.js
@@ -0,0 +1,7 @@
+const math = require("./math.js");
+
+console.log(math.add(1, 0));
+console.log(math.substract(2, 1));
+console.log(math.multiply(1, 0));
+console.log(math.divide(1, 0));
+console.log(math.divide(2, 1));
